bec9322e27b31f051142749d265b3910722d13e7
Salih Akşit <salih.aksit7@gmail.com>
2018-06-18 14:02:52 +0300

    for ios and android

--- stringparser.py
+++ stringparser.py
@@ -4,34 +4,38 @@ import os
 import fnmatch
 import xlrd
 import xml.etree.ElementTree as ET  
+import io
 
-def dir_list_folder(head_dir, dir_name):
+myKeysAndValues = []
+
+def dir_list_folder(head_dir, file_name):
     outputList = []
     for root, dirs, files in os.walk(head_dir):
-    	for filename in fnmatch.filter(files, 'strings.xml'):
+    	for filename in fnmatch.filter(files, file_name):
         	outputList.append(os.path.join(root, filename))
-        # for d in dirs:
-        #     if dir_name in d:
-        #         outputList.append(os.path.join(root, d))
     return outputList
 
 def askForProjectPath():
 	return raw_input("Write the project path (Eg: /Users/mobilion/DemoProject) : \n")
 
+def askForProjectPathiOS():
+	return raw_input("Write the project's Assets path (Eg: /Users/mobilion/DemoProjectIos/DemoProject/Assets) : \n")
+
 def askForExcelName():
 	return raw_input("Write the excel name (Eg: DemoProject) : \n")
 
 def askForDirection():
-	answer = raw_input("Write E to xml -> excel\nWrite X to excel -> xml : \n")
-	if answer == "E":
+	answer = raw_input("Write X to xml -> excel\nWrite E to excel -> xml & strings \nWrite S to strings -> excel : \n")
+	if answer == "X":
 		xmlToExcel()
-	elif answer == "X":
-		excelToXml()
+	elif answer == "E":
+		excelToBoth()
+	elif answer == "S":
+		stringsToExcel()
 	else :
-		print("Run again. Please write only E or X.")
+		print("Run again")
 
-def excelToXml():
-	dataExcel = xlrd.open_workbook(askForExcelName() + '.xls')
+def excelToXml(dataExcel):
 	table = dataExcel.sheets()[0]
 	for j in range(table.ncols):
 		if j + 1 < table.ncols:
@@ -48,7 +52,22 @@ def excelToXml():
 			if not os.path.exists(folderName):
 				os.makedirs(folderName) 
 			myfile = open(folderName + "/strings.xml", "w")  
-			myfile.write(mydata)  
+			myfile.write(mydata)
+			myfile.close()
+
+def excelToStrings(dataExcel):
+	table = dataExcel.sheets()[0]
+	for j in range(table.ncols):
+		if j + 1 < table.ncols:
+			folderName = table.cell_value(0, j + 1) + ".lproj"
+			if not os.path.exists(folderName):
+				os.makedirs(folderName) 
+			myfile = io.open(folderName + "/Localizable.strings", encoding='utf_8', mode='w')   
+			for i in range(table.nrows):
+				if i > 0:
+					mydata = "\"" + table.cell_value(i, 0) + "\" = \"" + table.cell_value(i, j + 1) + "\";\n"
+					myfile.write(mydata)
+			myfile.close()
 
 def xmlToExcel():
 	# Set first values
@@ -56,7 +75,7 @@ def xmlToExcel():
 	col = 0
 	tempItems = []
 	allNameAttributes = []
-	paths = dir_list_folder(askForProjectPath() + '/app/src/main/res/', 'values')
+	paths = dir_list_folder(askForProjectPath() + '/app/src/main/res/', 'strings.xml')
 	excelName = askForExcelName()
 
 	if len(paths) != 0:
@@ -86,5 +105,52 @@ def xmlToExcel():
 		workbook.close()
 	else :
 		print("No path found")
+
+def appendString(line):
+	myKeysAndValues.append( [n.replace("\"", "") for n in line.strip().split('=')])
+
+def stringsToExcel():
+	# Set first values
+	row = 0
+	col = 0
+	allNameAttributes = []
+	paths = dir_list_folder(askForProjectPathiOS(), 'Localizable.strings')
+	
+
+	if len(paths) != 0:
+		excelName = askForExcelName()
+		# Create a workbook and add a worksheet.
+		workbook = xlsxwriter.Workbook(excelName + '.xls')
+		worksheet = workbook.add_worksheet()
+
+		indexOfTempItems = 1
+		# Get all string tags
+		for path in paths:
+			lines = open(path).read().split('\n')
+			for line in lines:
+				appendString(line)
+				for elem in myKeysAndValues:
+					if len(elem) == 2 :
+						tempList = [i for i,tempElem in enumerate(allNameAttributes) if tempElem == elem[0]]
+					  	actualRow = 0
+						if len(tempList) == 0:
+							actualRow = row
+							worksheet.write(actualRow, col,     elem[0])
+							allNameAttributes.append(elem[0])
+							row += 1
+						elif len(tempList) == 1:
+							actualRow = tempList[0]
+						else :
+							break 
+						worksheet.write(actualRow, col + indexOfTempItems, elem[1].decode('utf-8'))
+			indexOfTempItems += 1
+		workbook.close()
+	else :
+		print("No path found")
+	
+def excelToBoth():
+	dataExcel = xlrd.open_workbook(askForExcelName() + '.xls')
+	excelToXml(dataExcel)
+	excelToStrings(dataExcel)
 
 askForDirection()
